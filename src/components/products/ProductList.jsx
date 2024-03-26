f35c89fd78c768498661cc018c9f3a5c21a5ab6c
CemilZemheri <cemilzemheri@gmail.com>
2024-03-26 08:58:31 +0300

    product list

---
+++ src/components/products/ProductList.jsx
@@ -0,0 +1,16 @@
+import ProductCard from "./ProductCard";
+import "./Products.scss";
+const ProductList = ({ products }) => {
+  console.log(products);
+  return (
+    <div className="products-list">
+    
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+
+      
+    </div>
+  );
+};
+export default ProductList;
